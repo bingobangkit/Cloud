04b90f25af57b56d3622f82949e0d32d67c71e4d
Fandi Syamsudin C0040345 <fandisyamsudin20@gmail.com>
2021-06-01 19:05:08 +0700

    Edit

--- Gobin-Web/app.py
+++ Gobin-Web/app.py
@@ -1,4 +1,3 @@
-### Script for CS329s ML Deployment Lec 
 import os
 import json
 import requests
@@ -7,30 +6,17 @@ import streamlit as st
 import tensorflow as tf
 from utils import load_and_prep_image, classes_and_models, update_logger, predict_json
 
-# Setup environment credentials (you'll need to change these)
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "go-bin-capstone-cc319e212459.json" # change for your GCP key
-PROJECT = "go-bin-capstone" # change for your GCP project
-REGION = "asia-southeast1" # change for your GCP region (where your model is hosted)
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "go-bin-capstone-cc319e212459.json" # credential service account
+PROJECT = "go-bin-capstone" # ID Project
+REGION = "asia-southeast1" # Region host model
 
-### Streamlit code (works as a straigtht-forward script) ###
 st.title("Welcome to Gobin Website")
 st.header("Identify what's in your plastic code!!!")
 
-@st.cache # cache the function so predictions aren't always redone (Streamlit refreshes every click)
+@st.cache 
 def make_prediction(image, model, class_names):
-    """
-    Takes an image and uses model (a trained TensorFlow model) to make a
-    prediction.
-
-    Returns:
-     image (preproccessed)
-     pred_class (prediction class from class_names)
-     pred_conf (model confidence)
-    """
     image = load_and_prep_image(image)
-    # Turn tensors into int16 (saves a lot of space, ML Engine has a limit of 1.5MB per request)
     image = tf.cast(tf.expand_dims(image, axis=0), tf.int16)
-    # image = tf.expand_dims(image, axis=0)
     preds = predict_json(project=PROJECT,
                          region=REGION,
                          model=model,
@@ -39,15 +25,14 @@ def make_prediction(image, model, class_names):
     pred_conf = tf.reduce_max(preds[0])
     return image, pred_class, pred_conf
 
-# Pick the model version
+
 choose_model = st.sidebar.selectbox(
     "Pick model you'd like to use",
-    ("Model 1 (10 food classes)", # original 10 classes
-     "Model 2 (11 food classes)", # original 10 classes + donuts
-     "Model 3 (11 food classes + non-food class)") # 11 classes (same as above) + not_food class
+    ("Model 1 (10 food classes)", 
+     "Model 2 (11 food classes)", 
+     "Model 3 (11 food classes + non-food class)") 
 )
 
-# Model choice logic
 if choose_model == "Model 1 (10 food classes)":
     CLASSES = classes_and_models["model_1"]["classes"]
     MODEL = classes_and_models["model_1"]["model_name"]
@@ -58,19 +43,14 @@ else:
     CLASSES = classes_and_models["model_3"]["classes"]
     MODEL = classes_and_models["model_3"]["model_name"]
 
-# Display info about model and classes
 if st.checkbox("Show classes"):
     st.write(f"You chose {MODEL}, these are the classes of food it can identify:\n", CLASSES)
 
-# File uploader allows user to add their own image
 uploaded_file = st.file_uploader(label="Upload an image of plastic code",
                                  type=["png", "jpeg", "jpg"])
 
-# Setup session state to remember state of app so refresh isn't always needed
-# See: https://discuss.streamlit.io/t/the-button-inside-a-button-seems-to-reset-the-whole-app-why/1051/11 
 session_state = SessionState.get(pred_button=False)
 
-# Create logic for app flow
 if not uploaded_file:
     st.warning("Please upload an image.")
     st.stop()
@@ -79,17 +59,14 @@ else:
     st.image(session_state.uploaded_image, use_column_width=True)
     pred_button = st.button("Predict")
 
-# Did the user press the predict button?
 if pred_button:
     session_state.pred_button = True 
 
-# And if they did...
 if session_state.pred_button:
     session_state.image, session_state.pred_class, session_state.pred_conf = make_prediction(session_state.uploaded_image, model=MODEL, class_names=CLASSES)
     st.write(f"Prediction: {session_state.pred_class}, \
                Confidence: {session_state.pred_conf:.3f}")
 
-    # Create feedback mechanism (building a data flywheel)
     session_state.feedback = st.selectbox(
         "Is this correct?",
         ("Select an option", "Yes", "No"))
@@ -97,7 +74,6 @@ if session_state.pred_button:
         pass
     elif session_state.feedback == "Yes":
         st.write("Thank you for your feedback!")
-        # Log prediction information to terminal (this could be stored in Big Query or something...)
         print(update_logger(image=session_state.image,
                             model_used=MODEL,
                             pred_class=session_state.pred_class,
@@ -107,14 +83,9 @@ if session_state.pred_button:
         session_state.correct_class = st.text_input("What should the correct label be?")
         if session_state.correct_class:
             st.write("Thank you for that, we'll use your help to make our model better!")
-            # Log prediction information to terminal (this could be stored in Big Query or something...)
             print(update_logger(image=session_state.image,
                                 model_used=MODEL,
                                 pred_class=session_state.pred_class,
                                 pred_conf=session_state.pred_conf,
                                 correct=False,
                                 user_label=session_state.correct_class))
-
-# TODO: code could be cleaned up to work with a main() function...
-# if __name__ == "__main__":
-#     main()
