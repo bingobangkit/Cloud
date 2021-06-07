f2fe109c7ec67aff860d2acb23d04064b281da71
Fandi Syamsudin C0040345 <fandisyamsudin20@gmail.com>
2021-06-07 13:33:41 +0700

    Re-design Web

--- app.py
+++ app.py
@@ -1,23 +1,26 @@
 import os
 import json
 import requests
-import SessionState
+import state
 import streamlit as st
 import tensorflow as tf
-from utils import load_and_prep_image, classes_and_models, update_logger, predict_json
+# from utils import load_and_prep_image, classes_and_models, update_logger, predict_json
+from predict import predict
+from image import preprocess_image
+# from logger import result_logger
 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "go-bin-capstone-1516532ab6ab.json" # credential service account
 PROJECT = "go-bin-capstone" # ID Project
 REGION = "asia-southeast1" # Region host model
 
-st.title("Welcome to Gobin Website")
+st.title("Gobin Web")
 st.header("Identify what's in your plastic code!!!")
 
 @st.cache 
 def make_prediction(image, model, class_names):
-    image = load_and_prep_image(image)
+    image = preprocess_image(image)
     image = tf.cast(tf.expand_dims(image, axis=0), tf.int16)
-    preds = predict_json(project=PROJECT,
+    preds = predict(project=PROJECT,
                          region=REGION,
                          model=model,
                          instances=image)
@@ -25,67 +28,37 @@ def make_prediction(image, model, class_names):
     pred_conf = tf.reduce_max(preds[0])
     return image, pred_class, pred_conf
 
+plastic_class = ['1_polyethylene_PET',
+ '2_high_density_polyethylene_PE-HD',
+ '3_polyvinylchloride_PVC',
+ '4_low_density_polyethylene_PE-LD',
+ '5_polypropylene_PP',
+ '6_polystyrene_PS',
+ '7_other_resins',
+ '8_no_plastic']
 
-choose_model = st.sidebar.selectbox(
-    "Pick model you'd like to use",
-    ("Model 1", 
-     "Model 2", 
-     "Model 3") 
-)
+plastic_model = "fandi_efficientnet_model"
 
-if choose_model == "Model 1":
-    CLASSES = classes_and_models["model_1"]["classes"]
-    MODEL = classes_and_models["model_1"]["model_name"]
-elif choose_model == "Model 2":
-    CLASSES = classes_and_models["model_2"]["classes"]
-    MODEL = classes_and_models["model_2"]["model_name"]
-else:
-    CLASSES = classes_and_models["model_3"]["classes"]
-    MODEL = classes_and_models["model_3"]["model_name"]
+if st.checkbox("Show plastic code?"):
+    st.write(f"Your model is {plastic_model}, the plastic code have 8 classes:\n", plastic_class)
 
-if st.checkbox("Show classes"):
-    st.write(f"You chose {MODEL}, these are the classes of plastic code it can identify:\n", CLASSES)
-
-uploaded_file = st.file_uploader(label="Upload an image of plastic code",
+uploaded_file = st.file_uploader(label="Upload your plastic code an image",
                                  type=["png", "jpeg", "jpg"])
 
-session_state = SessionState.get(pred_button=False)
+session_state = state.get(pred_button=False)
 
 if not uploaded_file:
-    st.warning("Please upload an image.")
+    st.warning("Please upload your plastic code an image")
     st.stop()
 else:
     session_state.uploaded_image = uploaded_file.read()
     st.image(session_state.uploaded_image, use_column_width=True)
-    pred_button = st.button("Predict")
+    pred_button = st.button("Predict image")
 
 if pred_button:
     session_state.pred_button = True 
 
 if session_state.pred_button:
-    session_state.image, session_state.pred_class, session_state.pred_conf = make_prediction(session_state.uploaded_image, model=MODEL, class_names=CLASSES)
+    session_state.image, session_state.pred_class, session_state.pred_conf = make_prediction(session_state.uploaded_image, model=plastic_model, class_names=plastic_class)
     st.write(f"Prediction: {session_state.pred_class}, \
                Confidence: {session_state.pred_conf*10:.2f}%")
-
-    session_state.feedback = st.selectbox(
-        "Is this correct?",
-        ("Select an option", "Yes", "No"))
-    if session_state.feedback == "Select an option":
-        pass
-    elif session_state.feedback == "Yes":
-        st.write("Thank you for your feedback!")
-        print(update_logger(image=session_state.image,
-                            model_used=MODEL,
-                            pred_class=session_state.pred_class,
-                            pred_conf=session_state.pred_conf,
-                            correct=True))
-    elif session_state.feedback == "No":
-        session_state.correct_class = st.text_input("What should the correct label be?")
-        if session_state.correct_class:
-            st.write("Thank you for that, we'll use your help to make our model better!")
-            print(update_logger(image=session_state.image,
-                                model_used=MODEL,
-                                pred_class=session_state.pred_class,
-                                pred_conf=session_state.pred_conf,
-                                correct=False,
-                                user_label=session_state.correct_class))
